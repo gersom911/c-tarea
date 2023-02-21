a659c1381d5a96f04ba388d3a50ca7fca393d44e
gersom911 <familiacedram@gmail.com>
2023-02-21 08:46:10 -0500

    ingrese nombre y fecha

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("escribe tu nombre: ");
+
+string name = Console.ReadLine();
+
+
+
+
+Console.WriteLine("escribe la hora: ");
+
+string hora = Console.ReadLine();
+
+Console.WriteLine("hola " + name);
+
+Console.WriteLine("la hora es  " + hora);
